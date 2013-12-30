0c95e8b070fa204e84a1c57bcdd34e503ebaeecf
Alex Cheng <chengxin1985@gmail.com>
2013-12-30 17:33:51 +0800

    add statistic data in layout with fake data.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,12 @@
 class WelcomeController < ApplicationController
+  before_filter :init_statistic
+
   def index
+  end
+
+  private
+  def init_statistic
+    @registered_user_count = 100
+    @unregistered_user_count = 50
   end
 end
